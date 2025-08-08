98726346af4fd559b32fc1950a0c918ca75967f9
zuweirayussif649-stack <zuweirayussif694@gmail.com>
2025-08-08 10:38:52 +0000

    z initial commit

---
+++ question1.py
@@ -0,0 +1,49 @@
+customer_name = "zuweira yussif"
+clean_name = customer_name.strip()
+lowercase_name = clean_name.lower()
+titlecase_name = clean_name.title()
+print("original name:", customer_name)
+print("cleaned name:", clean_name)
+print("lowercase name:",lowercase_name)
+print("titlecase name:", titlecase_name)
+print("lenght of name:", len(clean_name))
+print("first character:", clean_name[0])
+print("last character:", clean_name[-1])
+print("first 5 numbers:", clean_name[:5])
+print("last 5 numbers:", clean_name[-5:])
+
+book_title = "the great circus in India"
+formatted_title = book_title.capitalize()
+print("original book title:", book_title)
+print("formatted book title:", formatted_title)
+
+product_code = "bj-345-NOVEL"
+uppercase_code = product_code.upper()
+new_seperator_code = uppercase_code.replace("-", "_")
+print("original product code:", product_code)
+print("uppercase product code:", uppercase_code)
+
+review = "This book is great. I love it!"
+book_count = review.count("book")
+print("review:", review)
+
+serial_number = "4567839741"
+is_digit_only =serial_number.isdigit()
+print("serial number:", serial_number)
+print("is serial number digit only?", is_digit_only)
+
+url_parts = ["the", "book","nook","online"]
+url_strings = "-".join(url_parts)
+sentence_string = " ".join(url_parts)
+print("URL string;", url_strings)
+print("Sentence string:", sentence_string)
+
+offer_code = "FREESHIPPING"
+is_offer_code_uppercase = offer_code.isupper()
+print("offer code:", offer_code)
+print("is offer code uppercase?", is_offer_code_uppercase)
+
+feedback_message = "I am very happy with your service."
+message_lenght = len(feedback_message)
+print("feedback message;", feedback_message)
+print("feedback message lenght:", message_lenght)
